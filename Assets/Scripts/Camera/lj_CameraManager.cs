6849f057c36dff5feea8b6a069f7e226e8612f17
Lawrence John Balanza <ljdb1999@gmail.com>
2024-12-19 20:35:33 -0800

    12.19.2024
    Transfering files to another machine 12/19 8pm

--- Assets/Scripts/Camera/lj_CameraManager.cs
+++ Assets/Scripts/Camera/lj_CameraManager.cs
@@ -7,7 +7,8 @@ public class lj_CameraManager : MonoBehaviour
     public Transform targetTransform; //object the camera will follow
     public Vector3 cameraFollowVelocity = Vector3.zero;
     public float cameraFollowSpeed = 0.2f;
-
+    public float lookAngle; //look up and down
+    public float pivotAngle; //look left and right
 	public void Awake()
 	{
         targetTransform = FindObjectOfType<lj_PlayerManager>().transform;
@@ -17,5 +18,10 @@ public class lj_CameraManager : MonoBehaviour
     {
         Vector3 targetPosition = Vector3.SmoothDamp(transform.position, targetTransform.position, ref cameraFollowVelocity, cameraFollowSpeed);
         transform.position = targetPosition;
+    }
+
+    public void RotateCamera()
+    {
+
     }
 }
